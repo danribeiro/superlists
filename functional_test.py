597a4bb5707575eed38a58b12005e1aa7a6ee9fc
Danilo <danribeiro.u2@gmail.com>
2022-02-02 15:04:14 -0300

    Teste funcional agora verifica se podemos inserir um item

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,12 @@
+import os
+import time
 import unittest
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.service import Service
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 s =  Service("/Applications/Firefox.app/Contents/MacOS/firefox")
 
 
@@ -8,17 +14,35 @@ class NewVisitorTest(unittest.TestCase):
     
     def setUp(self):
         self.browser = webdriver.Firefox(
-            executable_path="/Users/daniloribeirosoares/Documents/projetos/superlists/geckodriver"
+            executable_path = BASE_DIR + "/geckodriver"
         )
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_ad_retrieve_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://127.0.0.1:8000")
-
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+    
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        inputbox.send_keys('Buy peacok feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: By peacock feathers' for row in rows )
+        )
+
+        self.fail('Finish the test')
+
 
 if __name__ == '__main__':
     unittest.main()
